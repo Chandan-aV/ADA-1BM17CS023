fa273d504915a329b0b5509aa04e3a901fbd2bf2
Chandan-aV <53899448+Chandan-aV@users.noreply.github.com>
2019-11-07 15:20:25 +0530

    Add files via upload

---
+++ coinss.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define INF 999
+#define N 4
+#define A 36
+void coinChange(int d[N+1], int C[A+1], int S[A+1]);
+void coinSet(int d[N+1], int S[A+1]);
+int main(void) {
+ 
+  int d[N+1] = {0, 1, 2, 5,10};
+  int C[A+1];
+  int S[A+1];
+  coinChange(d, C, S);
+printf("\nMin. no. of coins required to make change for amount %d = %d\n", A, C[A]);
+  coinSet(d, S);
+return 0;
+}
+void coinChange(int d[N+1], int C[A+1], int S[A+1]) {
+  int i, p, min, coin;
+  C[0] = 0;
+  S[0] = 0;
+  
+  for(p = 1; p <= A; p++) {
+    min = INF;
+    for(i = 1; i <= N; i++) {
+      if(d[i] <= p) {
+        if(1 + C[p - d[i]] < min) {
+          min = 1 + C[p - d[i]];
+          coin = i;
+        }
+      }
+    }
+    C[p] = min;
+    S[p] = coin;
+  }
+}
+
+void coinSet(int d[N+1], int S[A+1]) {
+	printf("coins are:\n");
+  int a = A;
+  while(a > 0) {
+    printf("%d\t", d[S[a]]);
+    a = a - d[S[a]];
+  }
+}
